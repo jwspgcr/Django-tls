9ba3b59c534aeb6b02f4e0ae2ec5fa709f092aa8
jwspgcr <syaga.works@gmail.com>
2021-02-19 01:32:46 +0900

    second

--- siritori/views.py
+++ siritori/views.py
@@ -1,5 +1,6 @@
 import random
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
 
 # def returnWord(request):
 #     textItems = len(request.text.split(" "))
@@ -16,8 +17,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 #             return HttpResponse("continue OK")
 
 def returnWord(request):
-    [word, command] = request.text.split(" ")
-    return HttpResponse("word:"+word+",command:"+command)
+    return HttpResponse("aaa")
+    # [word, command] = request.text.split(" ")
+    # return HttpResponse("word:"+word+",command:"+command)
 
 # def validateWord(word):
     # 三文字か、んで終わっていないか、辞書にあるか、もう使われていないか
