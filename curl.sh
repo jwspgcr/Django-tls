a6c7355fbb697cf163dc97d188465d36f3af8c88
jwspgcr <syaga.works@gmail.com>
2021-02-22 23:23:06 +0900

    17

---
+++ curl.sh
@@ -0,0 +1,2 @@
+curl -X POST -d 'text=あぎと&command=//tls-add' -s 127.0.0.1:8000
+="{""model"":""siritori.word"",""pk"":"&A1&",""fields"":{""kana"":"""&mid(B1,1,3)&""",""kanji"":"""&mid(B1,4,100)&"""}},"
---
+++ registerWordsmini.py
@@ -0,0 +1,3 @@
+from siritori.models import Word
+Word(kana="ああす",kanji="アース").save()
+Word(kana="ああち",kanji="アーチ").save()
